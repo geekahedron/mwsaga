9c5ac705b6853d26268ff22da86b4ff7c57fd2cb
admin <admin@itlaptop12.mohawkcasino.int>
2019-11-07 12:15:42 -0500

    Initial merge-commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+	const app = express()
+	const server=require('http').createServer(app)
+
+	app.use(function(req,res,next)
+	{
+		let out = "This is a test page (" + app.get('env') + "," + req.path + ")"
+		res.send(out)
+	})
+
+	// Start server
+	let port = process.env.PORT || 3000
+	server.listen(port, function () {
+	  console.log('Express server listening on port %d in %s mode', port, app.get('env'))
+	})
